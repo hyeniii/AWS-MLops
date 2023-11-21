d8159a3682e06b60a5e2bc07443f8f19150d7457
Sharika's MacAir <sharikamahadevan2023@u.northwestern.edu>
2023-11-20 20:21:37 -0600

    Updated Client and api_make_description files

--- server-files/lambda_api_make_description_docker/make_description.py
+++ server-files/lambda_api_make_description_docker/make_description.py
@@ -3,6 +3,7 @@
 from langchain.llms import OpenAI
 import openai
 from configparser import ConfigParser
+import json
 import os
 
 def lambda_handler(event, context):
@@ -14,39 +15,60 @@ def lambda_handler(event, context):
     print(os.environ["OPENAI_API_KEY"])
 
     try:
+        print("We are inside the lambda.")
+
+        # print(event)
+
+        # if "body" not in event:
+        #     return "event has no body"
+            # raise Exception("event has no body")
+        
+        # body = json.loads(event["body"]) # parse the json
+        
         # Accessing the incoming data
         bedrooms = event["bedrooms"]
         bathrooms = event["bathrooms"]
         square_feet = event["square_feet"]
         cityname = event["cityname"]
         has_photos = None
-        if event["has_photos"] == "Yes":
+        if event["has_photos"] == "Yes": 
             has_photos = "There are display pictures."
         else:
             has_photos = "There are no display pictures."
 
-        pets_allowed = None
-        if event["pets_allowed"] == "Yes":
-            pets_allowed = "It is pet-friendly."
+        dogs_allowed = None
+        if event["dogs_allowed"] == "Yes":
+            dogs_allowed = "It is dog-friendly."
         else:
-            pets_allowed = "It is not pet-friendly."
+            dogs_allowed = "It is not dog-friendly."
+
+        cats_allowed = None
+        if event["cats_allowed"] == "Yes":
+            cats_allowed = "It is cat-friendly."
+        else:
+            cats_allowed = "It is not cat-friendly."
+        
+        amenities = "The amenities available are " + ", ".join(event["amenities"]) + "."
 
         print(bedrooms)
         print(bathrooms)
         print(square_feet)
         print(cityname)
         print(has_photos)
-        print(pets_allowed)
+        print(dogs_allowed)
+        print(cats_allowed)
+        print(amenities)
     except Exception as err:
         print("An error has occured while generating the response.")
         print(err)
+        # return err
         return {
             'statusCode': 400,
             'description': err
         }
 
     # Create the prompt for LLM
-    prompt = f"There is a house with {bedrooms} bedrooms, {bathrooms} bathrooms with an area of {square_feet} sq. feet. It is located in {cityname}. {pets_allowed} {has_photos}. Make a description for a rental posting on Zillow."
+    prompt = f"There is a house with {bedrooms} bedrooms, {bathrooms} bathrooms with an area of {square_feet} sq. feet. It is located in {cityname}. {dogs_allowed} {cats_allowed} {has_photos}. {amenities} Make a description for a rental posting on Zillow."
     model = "gpt-3.5-turbo"
     num_tokens = 100
     print(prompt)
